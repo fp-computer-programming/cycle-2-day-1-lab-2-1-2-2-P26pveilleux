599c8ee932e09bc6da129490f5e9ee3bdecd6070
P26pveilleux <144148685+P26pveilleux@users.noreply.github.com>
2023-09-14 10:58:26 -0400

    I followed the steps that were given to me and completed the assigment

--- lab2-2.py
+++ lab2-2.py
@@ -1,13 +1,16 @@
 # 1) Create a Python file named lab_2-2.py
 
 # 2) Set a equal to 3 and b equal to a
-
+a = 3
+b = a 
 # 3) Print the value of b and the value of a.
-
+print (b,a)
 # 4) What is the value of each variable and why? (Answer as a comment)
-
+# a is equall to b which 3 so they equall 3 
 # 5) Set a equal to 7.
-
+a = 7 
 # 6) Print the value of a and the value of b
-
+print (a)
+print (b)
 # 7) What is the value of each variable and why? (Answer as a comment)
+#both are eqaull to 7 
